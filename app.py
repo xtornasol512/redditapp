e8e8770bd3fabeaba26efe09f950d7498908ef2d
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2017-08-08 01:27:37 -0500

    add reddit variables

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import os
 from flask import Flask, render_template
 
+# Reddit Config
+CLIENT_ID = os.environ["REDDIT_CLIENT_ID"]
+CLIENT_SECRET = os.environ["REDDIT_CLIENT_SECRET"]
+REDIRECT_URI = "https://{}.herokuapp.com/{}".format(
+    os.environ["HEROKU_APP_NAME"],
+    os.environ["REDDIT_REDIRECT_URI"]
+    )
+
 app = Flask(__name__)
 
 @app.route("/")
